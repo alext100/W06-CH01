8ef46f8abeca3fe73bdb350b92b27f5b327e1a32
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-01 21:22:01 +0100

    React Bootstrap form for task input added

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { FloatingLabel, Form } from "react-bootstrap";
 import useToDos from "../hooks/useToDo";
 
 const MainPage = ({ toDo }) => {
@@ -8,6 +9,29 @@ const MainPage = ({ toDo }) => {
     loadTasks();
   }, [loadTasks]);
 
-  return <h1>ToDo List</h1>;
+  const createTask = () => {};
+
+  return (
+    <>
+      <h1>ToDo List</h1>
+      <Form className="form-create" autoComplete="off" onSubmit={createTask}>
+        <Form.Group className="col-md-4" controlId="task">
+          <Form.Label>New task to do:</Form.Label>
+          <FloatingLabel
+            controlId="task"
+            label="Buy some cookies"
+            className="mb-3"
+          >
+            <Form.Control
+              placeholder="Buy some cookies"
+              type="text"
+              /*    value={taskDataToUpdate.task}
+              onChange={changeData} */
+            />
+          </FloatingLabel>
+        </Form.Group>
+      </Form>
+    </>
+  );
 };
 export default MainPage;
