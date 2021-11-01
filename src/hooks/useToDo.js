ee27905a2ff6e8bd0f7d5790cca3a1674c4ec93b
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-01 20:35:12 +0100

    useToDos hook with method loadTasks() created

---
+++ src/hooks/useToDo.js
@@ -0,0 +1,15 @@
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getToDosThunk } from "../redux/thunks/thunks";
+
+const useToDos = () => {
+  const dispatch = useDispatch();
+  const { toDos } = useSelector(({ toDos }) => toDos);
+  const loadTasks = useCallback(() => {
+    dispatch(getToDosThunk());
+  }, [dispatch]);
+
+  return { toDos, loadTasks };
+};
+
+export default useToDos;
